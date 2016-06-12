1d419a3a776b169d82116ccdc4e438337d8d66b1
padmajakondeti <padma.kondeti@gmail.com>
2016-06-12 08:28:32 -0400

    added still and animate functionality

--- AnimalTastic/assets/js/animaltastic.js
+++ AnimalTastic/assets/js/animaltastic.js
@@ -9,7 +9,7 @@ function displayAnimalInfo(){
 	 + animal + "&limit=5&rating=pg&fmt=json";
 	 // Creates AJAX call for the specific animal being 
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-		var animalDiv = $('<div class="animal">');
+		var animalDiv = $('<div class="animalDiv">');
 		
 		// Creates a generic div to hold the animal
 		for (var i = 0; i < response.data.length; i++){
@@ -21,14 +21,15 @@ function displayAnimalInfo(){
 			animalDiv.append(pOne);
 			// Creates an element to hold the image 
 			var image = $('<img>').attr({
-				"data-still": response.data[i].images.fixed_height.url,
-				"data-animate": response.data[i].images.fixed_height_still.url,
+				"data-still": response.data[i].images.fixed_height_still.url,
+				"data-animate": response.data[i].images.fixed_height.url,
 				"data-state": "still",
 				"src": response.data[i].images.fixed_height_still.url,
 
 			}).addClass("animalImage img-thumbnail");
 			
 			animalDiv.append(image);
+			
 			// Puts the entire animal above the previous animals.
 		};
 		$('#animalsView').prepend(animalDiv);
@@ -44,7 +45,7 @@ function renderButtons(){
 		// Then dynamicaly generates buttons for each animal in the array
 		// Note the jQUery syntax here... 
 	    var a = $('<button>') // This code $('<button>') is all jQuery needs to create the beginning and end tag. (<button></button>)
-	    a.addClass('animal'); // Added a class 
+	    a.addClass('animalButton'); // Added a class 
 	    a.attr('data-name', animals[i]); // Added a data-attribute
 	    a.text(animals[i]); // Provided the initial button text
 	    $('#buttonsView').append(a); // Added the button to the HTML
@@ -57,35 +58,36 @@ $(document).ready(function(){
 		//if ($('animal-input').val().trim().length >0){
 			// This line of code will grab the input from the textbox
 			var animal = ($('#animal-input').val()).trim();
-			// The animal from the textbox is then added to our array
-			animals.push(animal);
-			// Our array then runs which handles the processing of our animal array
-			renderButtons();
-			// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
-			return false;
-		//} else {
-			//alert("please enter a value")
-		//};
+			if (animal.length > 0 ){
+				// The animal from the textbox is then added to our array
+				animals.push(animal);
+				// Our array then runs which handles the processing of our animal array
+				renderButtons();
+				// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+				return false;
+			} else {
+				alert("please enter your favorite animal")
+			};
 	})
 	// ========================================================
 	// Generic function for displaying the animalInfo
-	$(document).on('click', '.animal', displayAnimalInfo);
+	$(document).on('click', '.animalButton', displayAnimalInfo);
 
 	// ========================================================
 	// This calls the renderButtons() function
 	renderButtons();
 	$(document).on('click','.animalImage', function(event){
-		event.stopPropagation();
-		//debugger
+		//event.stopPropagation();
 		var state = $(this).attr('data-state');
 		if ( state == 'still'){
             $(this).attr('src', $(this).data('animate'));
             $(this).attr('data-state', 'animate');
+          	
         }else{
             $(this).attr('src', $(this).data('still'));
             $(this).attr('data-state', 'still');
+           
         }
-         
         //return false;
      });
 })
